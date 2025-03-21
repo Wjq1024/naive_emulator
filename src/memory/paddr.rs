7f2f68be75cd20884e67673743cc1a63cb87eead
GSteve-Paul <lijn101@nenu.edu.cn>
2025-03-21 23:50:49 +0800

    implement host_read and host_write

--- src/memory/paddr.rs
+++ src/memory/paddr.rs
@@ -1,3 +1,5 @@
+use core::panic;
+
 use crate::common::*;
 
 use super::MemoryManager;
@@ -7,12 +9,43 @@ impl MemoryManager {
         paddr as usize - MEM_OFFSET
     }
 
-    fn host_read(&self, host_index: usize, len: usize) -> Word {
-        unimplemented!()
+    fn check_host_addr(host_index: usize, len: usize) {
+        if host_index + len > PMEM_SIZE {
+            panic!("host_addr out of bounds, host_addr: {host_index}, len: {len}")
+        }
     }
 
-    fn host_write(&mut self, host_index: usize, len: usize) {
-        unimplemented!()
+    fn host_read(&self, host_index: usize, len: usize) -> Word {
+        Self::check_host_addr(host_index, len);
+        match (host_index, len) {
+            (l, 1) => self.mem[l] as u32,
+            (l, 2) => self.mem[l] as u32 + (self.mem[l + 1] as u32) << 8,
+            (l, 4) => {
+                self.mem[l] as u32 + (self.mem[l + 1] as u32)
+                    << 8 + (self.mem[l + 2] as u32)
+                    << 16 + (self.mem[l + 3] as u32)
+                    << 24
+            }
+            (_, _) => panic!("invalid len, host_addr: {host_index}, len: {len}"),
+        }
+    }
+
+    fn host_write(&mut self, host_index: usize, len: usize, data: Word) {
+        Self::check_host_addr(host_index, len);
+        match (host_index, len) {
+            (l, 1) => self.mem[l] = (data & ((1 << 8) - 1)) as u8,
+            (l, 2) => {
+                self.mem[l] = data as u8;
+                self.mem[l + 1] = (data >> 8) as u8;
+            }
+            (l, 4) => {
+                self.mem[l] = data as u8;
+                self.mem[l + 1] = (data >> 8) as u8;
+                self.mem[l + 2] = (data >> 16) as u8;
+                self.mem[l + 3] = (data >> 24) as u8;
+            }
+            (_, _) => panic!("invalid len, host_addr: {host_index}, len: {len}, data: {data}"),
+        }
     }
 }
 
@@ -20,6 +53,6 @@ pub fn paddr_read(paddr: PAddr, len: usize) -> Word {
     unimplemented!()
 }
 
-pub fn paddr_write(paddr: PAddr, len: usize) {
+pub fn paddr_write(paddr: PAddr, len: usize, data: Word) {
     unimplemented!()
 }
