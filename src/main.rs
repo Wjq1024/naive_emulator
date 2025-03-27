1c005f406072f9535f8e6489a3eb64a0452c391c
GSteve-Paul <lijn101@nenu.edu.cn>
2025-03-27 23:49:02 +0800

    add naive_load to test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use std::usize;
+
+use cpu::{CPU, cpu_run};
+use loader::naive_load;
+
 mod common;
 mod cpu;
+mod loader;
 mod memory;
 mod uniprocessor;
 
 fn main() {
-    println!("Hello, world!");
+    naive_load();
+    cpu_run(usize::MAX);
+    println!("{:?}", CPU.exclusive_access());
 }
