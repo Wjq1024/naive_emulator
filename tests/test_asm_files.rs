b97234eab6ddd88acf521d14bd2b1f7d12c4d491
Wjq <1377146983@qq.com>
2025-04-02 16:31:36 +0800

    test:补充注释说明测试结果的预期值
    - 在 test_trunc 求和测试中添加注释说明寄存器 1 的预期值 55 是 1 到 10 的和
    - 在 test_trunc 阶乘测试中将寄存器 1 的预期值120 的注释改为中文解释

--- tests/test_asm_files.rs
+++ tests/test_asm_files.rs
@@ -9,7 +9,7 @@ fn test_sum_asm() {
     cpu_run(usize::MAX);
     assert_eq!(cpu_gpr(2), 10);
     assert_eq!(cpu_gpr(3), 10);
-    assert_eq!(cpu_gpr(1), 55);
+    assert_eq!(cpu_gpr(1), 55); //1+...+10=55
 }
 
 #[test]
@@ -18,5 +18,5 @@ fn test_factorial_asm() {
     cpu_run(usize::MAX);
     assert_eq!(cpu_gpr(2), 6);  // 循环上限
     assert_eq!(cpu_gpr(3), 6);  // 循环计数器的最终值
-    assert_eq!(cpu_gpr(1), 120); // 阶乘结果
+    assert_eq!(cpu_gpr(1), 120); // 5！阶乘结果
 }
